305c5072ddbeeddb8586735502fabc08e4085803
Boyesz <boykahun7@gmail.com>
2017-10-08 17:09:56 +0200

    Implemented MenuItem,Ingredient in core.

--- restaurantApp-Core/src/main/java/com/github/dugasz1/szoftverteszteles/core/menu/Ingredient.java
+++ restaurantApp-Core/src/main/java/com/github/dugasz1/szoftverteszteles/core/menu/Ingredient.java
@@ -4,5 +4,34 @@ public class Ingredient {
     private IngredientItem ingredientItem;
     private float quantity;
 
-    //TODO:
+    /**
+     * Creates Ingredient object, do not use this use the dao interface.
+     * @param ingredientItem
+     * @param quantity
+     */
+    public Ingredient(IngredientItem ingredientItem, float quantity)
+    {
+        if(ingredientItem == null)
+            throw new NullPointerException("ingredientItem cannot be null.");
+        if(quantity < 0)
+            throw  new IllegalArgumentException("quantity cannot be negative.");
+        this.ingredientItem = ingredientItem;
+        this.quantity = quantity;
+    }
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public IngredientItem getIngredientItem() {
+        return ingredientItem;
+    }
+
+    public void setIngredientItem(IngredientItem ingredientItem) {
+        this.ingredientItem = ingredientItem;
+    }
+
+    public void setQuantity(float quantity) {
+        this.quantity = quantity;
+    }
 }
--- restaurantApp-Core/src/main/java/com/github/dugasz1/szoftverteszteles/core/menu/MenuItem.java
+++ restaurantApp-Core/src/main/java/com/github/dugasz1/szoftverteszteles/core/menu/MenuItem.java
@@ -4,4 +4,41 @@ public class MenuItem {
     private int id;
     private float price;
     private Recipe recipe;
+
+    /**
+     * Creates MenuItem object, do not use this use the dao interface.
+     * @param id
+     * @param price
+     * @param recipe
+     */
+    public MenuItem(int id,float price,Recipe recipe)
+    {
+        if(price < 0)
+            throw new IllegalArgumentException("Price cannot be negative.");
+        if(recipe == null)
+            throw new NullPointerException("Recipe cannot be null.");
+        this.id = id;
+        this.price = price;
+        this.recipe = recipe;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
 }
