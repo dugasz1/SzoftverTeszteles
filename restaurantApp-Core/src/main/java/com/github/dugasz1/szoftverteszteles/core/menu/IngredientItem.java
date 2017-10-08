69a8eaa29703489e4e682e7a8bd01e4b4bc0a753
Boyesz <boykahun7@gmail.com>
2017-10-08 15:43:17 +0200

    Implemented IngredientItem in core.
    Created getter,setter,constructor.

--- restaurantApp-Core/src/main/java/com/github/dugasz1/szoftverteszteles/core/menu/IngredientItem.java
+++ restaurantApp-Core/src/main/java/com/github/dugasz1/szoftverteszteles/core/menu/IngredientItem.java
@@ -3,7 +3,53 @@ package com.github.dugasz1.szoftverteszteles.core.menu;
 public class IngredientItem {
     private int id;
     private String name;
-    private Nutrions nutrions;
+    private Nutritions nutrions;
     private String unit;
 
+    /**
+     * Creates IngredientItem object, do not use this use the dao interface.
+     * @param id
+     * @param name
+     * @param nutritions
+     * @param unit
+     */
+
+    public IngredientItem(int id, String name, Nutritions nutritions, String unit)
+    {
+        if(name == null)
+            throw new NullPointerException("Name cannot be null.");
+        if(nutritions == null)
+            throw new NullPointerException("Ingredient must be contain one value.");
+        this.id = id;
+        this.name = name;
+        this.nutrions = nutritions;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Nutritions getNutrions() {
+        return nutrions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setNutrions(Nutritions nutrions) {
+        this.nutrions = nutrions;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
 }
