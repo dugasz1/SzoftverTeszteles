af6d3d0c2032c81dd0a0ebc03930a2b0a5cee9b0
Boyesz <boykahun7@gmail.com>
2017-10-08 16:10:40 +0200

    Impleneted Category in core

--- restaurantApp-Core/src/main/java/com/github/dugasz1/szoftverteszteles/core/menu/Category.java
+++ restaurantApp-Core/src/main/java/com/github/dugasz1/szoftverteszteles/core/menu/Category.java
@@ -4,7 +4,31 @@ public class Category {
     private int id;
     private String name;
 
+    /**
+     * Creates Category object, do not use this use the dao interface.
+     * @param id
+     * @param name
+     */
     public Category(int id, String name){
+        if(name == null)
+            throw new NullPointerException("Name cannot be null.");
+        this.id = id;
+        this.name = name;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
