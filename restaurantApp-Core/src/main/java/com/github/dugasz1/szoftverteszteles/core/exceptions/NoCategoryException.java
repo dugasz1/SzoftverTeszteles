d4c8c4285716e853770b1ae18a7f129ef1b98766
Boyesz <boykahun7@gmail.com>
2017-10-09 14:58:54 +0200

    Create exception for category
    NoCategoryException implemented

---
+++ restaurantApp-Core/src/main/java/com/github/dugasz1/szoftverteszteles/core/exceptions/NoCategoryException.java
@@ -0,0 +1,9 @@
+package com.github.dugasz1.szoftverteszteles.core.exceptions;
+
+public class NoCategoryException extends Exception {
+    public  NoCategoryException(String message){
+        super(message);
+    }
+
+
+}
