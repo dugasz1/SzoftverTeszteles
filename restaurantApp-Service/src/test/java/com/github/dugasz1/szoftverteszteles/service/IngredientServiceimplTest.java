7447b8e334e629a99f2e2247089c3da9363721af
Répási Kristóf <boykahun7@gmail.com>
2017-11-21 17:48:35 +0100

    Tested some implemented class in service
    Tested UserService and IndredientService Test

--- restaurantApp-Service/src/test/java/com/github/dugasz1/szoftverteszteles/service/IngredientServiceimplTest.java
+++ restaurantApp-Service/src/test/java/com/github/dugasz1/szoftverteszteles/service/IngredientServiceimplTest.java
@@ -1,5 +1,6 @@
 package com.github.dugasz1.szoftverteszteles.service;
 
+import com.github.dugasz1.szoftverteszteles.core.exceptions.NotFoundException;
 import com.github.dugasz1.szoftverteszteles.core.model.*;
 import com.github.dugasz1.szoftverteszteles.service.dao.IngredientDAO;
 import com.github.dugasz1.szoftverteszteles.service.dao.MenuDAO;
@@ -22,6 +23,11 @@ public class IngredientServiceimplTest {
         Nutritions nutritions = new Nutritions(2.2f,3.3f,4.4f,5.5f,6.6f);
         IngredientItem ingredientItem = new IngredientItem(1,"test",nutritions,"test");
         EasyMock.expect(ingredientDAO.getIngredientItem(1)).andReturn(ingredientItem);
+        EasyMock.expect(ingredientDAO.updateIngredientItem(1)).andReturn(false);
+        EasyMock.expect(ingredientDAO.updateIngredientItem(ingredientItem)).andReturn(true);
+        EasyMock.expect(ingredientDAO.deleteIngredientItem(2)).andThrow(new NotFoundException());
+        EasyMock.expect(ingredientDAO.deleteIngredientItem(1)).andReturn(true);
+        EasyMock.expect(ingredientDAO.deleteIngredientItem(ingredientItem)).andReturn(true);
         EasyMock.replay(ingredientDAO);
         ingredientServiceimpl = new IngredientServiceimpl(ingredientDAO);
     }
@@ -35,18 +41,31 @@ public class IngredientServiceimplTest {
 
     @Test
     public void testUpdateIngredientItem() throws Exception {
+        assertEquals(ingredientServiceimpl.updateIngredientItem(1),false);
     }
 
     @Test
     public void testUpdateIngredientItem1() throws Exception {
+        Nutritions nutritions = new Nutritions(2.2f,3.3f,4.4f,5.5f,6.6f);
+        IngredientItem ingredientItem = new IngredientItem(1,"test",nutritions,"test");
+        assertTrue(ingredientServiceimpl.updateIngredientItem(ingredientItem) == true);
     }
 
-    @Test
+    @Test(expected = NotFoundException.class)
     public void testDeleteIngredientItem() throws Exception {
+        ingredientServiceimpl.deleteIngredientItem(2);
+
+    }
+    @Test
+    public void testDeleteIngredientItem2() throws Exception{
+        assertTrue(ingredientServiceimpl.deleteIngredientItem(1)== true);
     }
 
     @Test
     public void testDeleteIngredientItem1() throws Exception {
+        Nutritions nutritions = new Nutritions(2.2f,3.3f,4.4f,5.5f,6.6f);
+        IngredientItem ingredientItem = new IngredientItem(1,"test",nutritions,"test");
+        assertEquals(ingredientServiceimpl.deleteIngredientItem(ingredientItem),true);
     }
 
 }
--- restaurantApp-Service/src/test/java/com/github/dugasz1/szoftverteszteles/service/UserServiceImplTest.java
+++ restaurantApp-Service/src/test/java/com/github/dugasz1/szoftverteszteles/service/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package com.github.dugasz1.szoftverteszteles.service;
 import com.github.dugasz1.szoftverteszteles.core.model.User;
 import com.github.dugasz1.szoftverteszteles.service.dao.UserDAO;
 import org.easymock.EasyMock;
+import org.easymock.internal.MocksControl;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,12 +12,17 @@ import static org.junit.Assert.*;
 
 public class UserServiceImplTest {
     private UserServiceImpl userService;
-
+    User teszt = new User(1,"teszt");
+    User uTeszt = new User(1,"uTeszt");
     @Before
     public void setUp() throws Exception {
         UserDAO userDAO = EasyMock.createNiceMock(UserDAO.class);
-        EasyMock.expect(userDAO.createUser("tesztuser")).andReturn(new User(1, "tesztuser"));
-
+        EasyMock.expect(userDAO.createUser("teszt")).andReturn(teszt);
+        EasyMock.expect(userDAO.getUser(1)).andReturn(teszt);
+        EasyMock.expect(userDAO.getUser("teszt")).andReturn(teszt);
+        EasyMock.expect(userDAO.updateUser(uTeszt)).andReturn(true);
+        EasyMock.expect(userDAO.deleteUser(1)).andReturn(true);
+        EasyMock.expect(userDAO.deleteUser(uTeszt)).andReturn(false);
         EasyMock.replay(userDAO);
 
         userService = new UserServiceImpl(userDAO);
@@ -24,29 +30,34 @@ public class UserServiceImplTest {
 
     @Test
     public void createUser() throws Exception {
-        User excepted = new User(1, "tesztuser");
-        User actual = userService.createUser("tesztuser");
+        User excepted = new User(1, "teszt");
+        User actual = userService.createUser("teszt");
         assertEquals(excepted, actual);
     }
 
     @Test
     public void getUser() throws Exception {
+        assertTrue(userService.getUser(1) == teszt);
     }
 
     @Test
     public void getUser1() throws Exception {
+        assertTrue(userService.getUser("teszt") == teszt);
     }
 
     @Test
     public void updateUser() throws Exception {
+        assertTrue(userService.updateUser(uTeszt) == true);
     }
 
     @Test
     public void deleteUser() throws Exception {
+        assertTrue(userService.deleteUser(1) == true);
     }
 
     @Test
     public void deleteUser1() throws Exception {
+        assertTrue(userService.deleteUser(uTeszt) == false);
     }
 
 }
